c70eafdbe5664fc1b392426a2ae553618b03f42e
Matt <matthewbertweb@gmail.com>
2021-01-06 14:15:56 -0500

    set length in/max for username and password

--- script.js
+++ script.js
@@ -50,6 +50,18 @@ function getFieldName(input) {
     return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
 
+// Check input length
+function checkLength(input, min, max) {
+    if(input.value.length < min) {
+        showError(input, `${getFieldName(input)} must be at least ${min} characters.`);
+    } else if(input.value.length > max) {
+        showError(input, `${getFieldName(input)} must be less than ${max} characters.`);
+    } else {
+        showSuccess(input);
+    }
+}
+
+
 // add an event listner on form when we submit it
 // function will have an event paramater, which has several properties to use
 form.addEventListener('submit', function(e) {
@@ -57,4 +69,6 @@ form.addEventListener('submit', function(e) {
     e.preventDefault();
 
     checkRequired([username, email, password, password2]);
+    checkLength(username, 3, 15);
+    checkLength(password, 6, 25);
 });
